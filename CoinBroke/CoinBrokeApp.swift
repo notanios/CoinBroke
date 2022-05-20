ae4344c99f0569b8d7ca4aeeba35f4af5e37dff5
Vlad D <vlad.drahnea@gmail.com>
2022-05-20 22:20:38 +0300

    Initial Commit

---
+++ CoinBroke/CoinBrokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinBrokeApp.swift
+//  CoinBroke
+//
+//  Created by vlad on 20.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoinBrokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
